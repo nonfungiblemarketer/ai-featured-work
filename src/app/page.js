de0b6d604f96b1d6d2da139df0b73e84a4e6bac3
stahaali07 <staha.ali07@gmail.com>
2023-11-30 18:37:18 +0500

    first commit

---
+++ src/app/page.js
@@ -0,0 +1,46 @@
+import styles from './page.module.css'
+import Project from '../components/project';
+
+export default function Home() {
+
+  const projects = [
+    {
+      title1: "Nutrition",
+      title2: "Center",
+      src: "nutrition-center.jpg"
+    },
+    {
+      title1: "Villa",
+      title2: "North",
+      src: "villa-north.jpg"
+    },
+    {
+      title1: "The Organic",
+      title2: "Store",
+      src: "organic-store.jpg"
+    },
+    {
+      title1: "Wahi Coffe",
+      title2: "Shop",
+      src: "nutrition-center.jpg"
+    },
+    {
+      title1: "Devanti",
+      title2: "Restaurant",
+      src: "villa-north.jpg"
+    }
+  ]
+
+  return (
+    <main className={styles.main}>
+      <div className={styles.gallery}>
+        <p>Featured Work</p>
+          {
+            projects.map( project => {
+              return <Project project={project}/>
+            })
+          }
+      </div>
+    </main>
+  )
+}
